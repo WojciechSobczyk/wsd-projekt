b0a1c908fba14c417003a17211f3c25dd2a22750
WojciechSobczyk <sobczyk-wojciech@wp.pl>
2019-12-14 19:41:49 +0100

    Added x,y UserAgent position parameters

--- src/main/java/com/albatrosy/wsd/agents/UserAgent.java
+++ src/main/java/com/albatrosy/wsd/agents/UserAgent.java
@@ -3,8 +3,21 @@ package com.albatrosy.wsd.agents;
 import jade.core.Agent;
 
 public class UserAgent extends Agent {
+
+    private Long x;
+    private Long y;
+
     @Override
     protected void setup() {
         super.setup();
+        initParameters();
+    }
+
+    private void initParameters() {
+        Object[] args = getArguments();
+        if (args.length != 2)
+            throw new IllegalStateException("UserAgent must have two arguments");
+        x = Long.parseLong(args[0].toString());
+        y = Long.parseLong(args[1].toString());
     }
 }
