475f93bb7613c2b11b951b09927d4356e736be7f
Mateusz <matnogall@gmail.com>
2019-12-14 20:31:13 +0100

    User agent send message

--- src/main/java/com/albatrosy/wsd/agents/UserAgent.java
+++ src/main/java/com/albatrosy/wsd/agents/UserAgent.java
@@ -1,5 +1,7 @@
 package com.albatrosy.wsd.agents;
-
+import jade.core.AID;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
 import jade.core.Agent;
 
 public class UserAgent extends Agent {
@@ -11,6 +13,7 @@ public class UserAgent extends Agent {
     protected void setup() {
         super.setup();
         initParameters();
+        addBehaviour(new Sender());
     }
 
     private void initParameters() {
@@ -19,5 +22,15 @@ public class UserAgent extends Agent {
             throw new IllegalStateException("UserAgent must have two arguments");
         x = Long.parseLong(args[0].toString());
         y = Long.parseLong(args[1].toString());
+    }
+    class Sender extends OneShotBehaviour {
+        @Override
+        public void action() {
+            ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+            msg.addReceiver(new AID("odbiorca", AID.ISLOCALNAME));
+            msg.setLanguage("Polish");
+            msg.setContent("Witaj");
+            send(msg);
+        }
     }
 }
