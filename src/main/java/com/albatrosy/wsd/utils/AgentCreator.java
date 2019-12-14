dadb5a2ec6f3bba7f85d688b12a668c0587651e2
Dawid Janik <jawddanik94@gmail.com>
2019-12-14 22:21:34 +0100

    basic agent creator

--- src/main/java/com/albatrosy/wsd/utils/AgentCreator.java
+++ src/main/java/com/albatrosy/wsd/utils/AgentCreator.java
@@ -28,8 +28,8 @@ public class AgentCreator extends Agent {
             @Override
             public void action() {
                 containerController = getContainerController();
-                createAgents(UserServiceDivisionAgent.class);
                 createAgents(UserAgent.class);
+                createAgents(UserServiceDivisionAgent.class);
             }
         });
     }
