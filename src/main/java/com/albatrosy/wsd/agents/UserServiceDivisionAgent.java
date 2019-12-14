8a21a751950c72648f2de18037a41c9ea475966c
WojciechSobczyk <sobczyk-wojciech@wp.pl>
2019-12-14 19:25:06 +0100

    Added Receiver to UserDivisionAgent

--- src/main/java/com/albatrosy/wsd/agents/UserServiceDivisionAgent.java
+++ src/main/java/com/albatrosy/wsd/agents/UserServiceDivisionAgent.java
@@ -1,6 +1,25 @@
 package com.albatrosy.wsd.agents;
 
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
 
 public class UserServiceDivisionAgent extends Agent {
+
+    @Override
+    protected void setup() {
+        super.setup();
+        addBehaviour(new Receiver());
+    }
+
+    class Receiver extends CyclicBehaviour {
+
+        @Override
+        public void action() {
+            ACLMessage message = receive();
+            if (message != null) {
+                System.out.println("Hello World");
+            }
+        }
+    }
 }
